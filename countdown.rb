35f68f092b328640ebadb07cf78fb28eab8b758c
Johannes Hupfeld <j.hupfeld@gmail.com>
2019-09-23 19:53:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 def countdown
 number = 10 
 while number >= 0
-puts "#{number} SECOND(S)"
+puts "#{number} SECOND(S)!"
 number = number - 1
 end 
 puts "HAPPY NEW YEAR"
@@ -12,7 +12,7 @@ countdown
 def countdown_with_sleep
 number = 10 
 while number >= 0
-puts "#{number} SECOND(S)"
+puts "#{number} SECOND(S)!"
 number = number - 1
 sleep (1) 
 end
